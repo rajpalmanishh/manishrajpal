3319ea81f436e62747db8d056bfe337387d1c9a7
baelesss <manishrajpal709@gmail.com>
2022-07-07 07:24:37 +0530

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo'))
+    {
+        document.querySelector('.cross').style.display='none';
+        document.querySelector('.ham').style.display='inline';
+    }
+    else
+    {
+        document.querySelector('.ham').style.display='none';
+        setTimeout(()=>{
+            document.querySelector('.cross').style.display='inline';
+        },300);
+    }
+})
